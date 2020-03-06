fadbf930050be43ff5ffa3f78775b1a47702dac4
Amber Volkmann <volkmannamber@gmail.com>
2020-03-06 15:28:46 -0600

    created router files

---
+++ server/routes/message.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const pool = require('../modules/pool');
+const userStrategy = require('../strategies/user.strategy');
+
+const router = express.Router();
+
+
+
+module.exports = router;
