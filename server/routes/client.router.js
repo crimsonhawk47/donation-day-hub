e094d7ae00f9bf61d8f61a82e1c1b7552b1e5d01
Amber Volkmann <volkmannamber@gmail.com>
2020-03-09 11:42:35 -0500

    created get routes for all teams, clients

--- server/routes/client.router.js
+++ server/routes/client.router.js
@@ -6,6 +6,18 @@ const userStrategy = require('../strategies/user.strategy');
 
 const router = express.Router();
 
-
+router.get('/', rejectUnauthenticated, (req, res) => {
+    const queryText = 'SELECT * FROM "client";'
+    console.log('in team router.get')
+    pool.query(queryText)
+        .then(result => {
+            console.log(result.rows)
+            res.send(result.rows)
+        }).catch(error => {
+            console.log('error in client GET', error)
+            res.sendStatus(500);
+        })
+            
+});
 
 module.exports = router;
