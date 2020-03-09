01540cf373672cbc146f94d846013926257bf65d
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-03-09 10:59:32 -0500

    Rendering Client Gallery

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,6 +16,7 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
+import ClientGallery from '../ClientGallery/ClientGallery'
 
 import './App.css';
 
@@ -38,6 +39,11 @@ class App extends Component {
               exact
               path="/about"
               component={AboutPage}
+            />
+            <Route
+              exact
+              path="/dev/gallery"
+              component={ClientGallery}
             />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
---
+++ src/components/ClientGallery/ClientGallery.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import { Paper, Grid, Typography } from '@material-ui/core'
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  }
+});
+
+class ClientGallery extends Component {
+
+  getImage = () => {
+    console.log(`GET IMAGE RAN`);
+    
+  }
+
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <>
+        <Grid container className={classes.root}>
+          <Typography >
+            I am a ClientGallery Component
+        </Typography>
+        </Grid>
+
+        <h1>Upload an image to AWS S3 bucket</h1>
+        <input
+          id='upload-image'
+          type='file'
+          accept='image/*'
+          onChange={this.getImage}
+        />
+        <form onSubmit={this.uploadFile}>
+          <button id='file-upload-button'>Upload</button>
+        </form>
+      </>
+    )
+
+  }
+}
+
+const mapStateToProps = reduxStore => {
+  return (
+    { reduxStore }
+  )
+}
+export default withStyles(styles)(connect(mapStateToProps)(ClientGallery))
