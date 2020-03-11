811a87313b60ca644f4f0a89b16d81f34264a3cf
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-03-11 01:44:45 -0500

    merge stash

--- src/redux/sagas/clientSaga.js
+++ src/redux/sagas/clientSaga.js
@@ -1,71 +1,14 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
-function* uploadToAws(action) {
-  console.log(action.payload);
+// worker Saga: will be fired on "FETCH_USER" actions
 
-  try {
-    let filename = action.payload.file.name
-    console.log(filename);
 
-    const config = {
-      headers: { 'Content-Type': 'application/json' },
-      params: {
-        client_id: action.payload.client_id,
-        Key: filename,
-        ContentType: 'image/jpeg'
-      },
-      withCredentials: true,
-    };
-
-    let response = yield axios.get('/api/aws/generate-put-url', config)
-    console.log(response);
-    if (response.data.code) throw `Generate get url failed with code ${response.data.code}`
-    let putUrl = response.data
-
-    let putResponse = yield axios.put(putUrl, action.payload.file)
-    if (putResponse.data.code) throw `Put image failed with code ${response.data.code}`
-    yield axios.post('/api/client/add-image-name', action.payload, config)
-  } catch (error) {
-    console.log('UPLOAD_TO_AWS ERROR', error);
-  }
-}
-
-function* getImageNames(action) {
-  // alert(`COMPLETE DISPLAYALLIMAGES SAGA LATER`);
-
-  const config = {
-    headers: { 'Content-Type': 'application/json' },
-    params: {
-      client_id: action.payload
-    },
-    withCredentials: true,
-  };
-  let response = yield axios.get('/api/client/list-of-images', config)
+function* getImageNames() {
+  let response = yield axios.get('/api/aws/list-of-images')
   console.log(response);
   yield put({ type: 'GET_MEDIA_FROM_NAMES', payload: response.data })
-  // yield put({type: 'SET_SELECTED_CLIENT_MEDIA', payload: response.data})
-}
-
-function* getMediaFromNames(action) {
-  let selectedMedia = []
-  for (let imageName of action.payload) {
-    const config = {
-      headers: { 'Content-Type': 'application/json' },
-      params: {
-        Key: imageName,
-        ContentType: 'image/jpeg'
-      },
-      withCredentials: true,
-    };
-
-    yield axios.get('/api/aws/generate-get-url', config).then(res => {
-      console.log(res);
-      selectedMedia = [...selectedMedia, res.data]
-    });
-    yield put({ type: 'SET_SELECTED_CLIENT_MEDIA', payload: selectedMedia })
-
-  }
+  yield put({ type: 'SET_SELECTED_CLIENT_MEDIA', payload: response.data})
 }
 
 function* getClientsById(action) {
@@ -79,9 +22,7 @@ function* getClientsById(action) {
 }
 
 function* clientSaga() {
-  yield takeLatest('UPLOAD_TO_AWS', uploadToAws);
   yield takeLatest('GET_IMAGE_NAMES', getImageNames);
-  yield takeLatest('GET_MEDIA_FROM_NAMES', getMediaFromNames)
   yield takeLatest('FETCH_CLIENTS_BY_TEAM', getClientsById)
 }
 
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -4,6 +4,7 @@ import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
 import clientSaga from './clientSaga';
 import teamSaga from './teamSaga';
+import awsSaga from './awsSaga'
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -19,5 +20,6 @@ export default function* rootSaga() {
     userSaga(),
     clientSaga(),
     teamSaga(),
+    awsSaga
   ]);
 }
