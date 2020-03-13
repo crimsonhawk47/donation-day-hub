5266a0937ec27077dbcfe21f60082b18da623002
Jessica Heggem <jessicaheggem@gmail.com>
2020-03-13 10:13:49 -0500

    volunteer info showing up on DOM

--- src/components/AdminVolunteerPage/AdminVolunteerPage.jsx
+++ src/components/AdminVolunteerPage/AdminVolunteerPage.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { withStyles } from '@material-ui/core/styles'
 import { Paper, Grid, Typography } from '@material-ui/core'
 
+const moment = require('moment');
+
 const styles = theme=> ({
   root: {
     flexGrow: 1,
@@ -14,9 +16,20 @@ class AdminVolunteerPage extends Component {
   render() {
     // const { classes } = this.props;
 
+  let volunteer = this.props.reduxStore.adminVolunteerInfo;
+
+
     return (
       <div>
-        <h1>Volunteer Page</h1>
+          <h1>{volunteer.first_name} {volunteer.last_name}</h1>
+          <p>Username: {volunteer.username}</p>
+          <p>Member since: {moment(volunteer.date_registered).format('LL')}</p>
+          <p>{volunteer.email}</p>
+          <p>{volunteer.phone}</p>
+          <p>{volunteer.street_address}</p>
+          <p>{volunteer.city}, {volunteer.state} {volunteer.zip}</p>
+
+
       </div>
     )
 
