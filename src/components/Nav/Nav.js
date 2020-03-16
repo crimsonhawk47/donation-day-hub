3d4e91271455789569abc573dc6540b115565ff4
Amber Volkmann <volkmannamber@gmail.com>
2020-03-16 17:26:47 -0500

    nav bar colors, fonts, and links edited

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -19,16 +19,17 @@ const Nav = (props) => (
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
         <>
-          <Link className="nav-link" to="/info">
-            Info Page
+          <Link className="nav-link" to="/team-page">
+            Team
+          </Link>
+          <Link className="nav-link" to="/resources">
+            Resources
           </Link>
           <LogOutButton className="nav-link" to="/home"/>
         </>
       )}
       {/* Always show this link since the about page is not protected */}
-      <Link className="nav-link" to="/about">
-        About
-      </Link>
+      
     </div>
   </div>
 );
