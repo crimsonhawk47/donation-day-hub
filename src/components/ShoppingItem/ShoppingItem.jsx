9076377f882fdbfcc1d7c6f3326e044c7de80509
Tou Xiong <touyeexiong@gmail.com>
2020-03-18 07:37:20 -0500

    buttons added for shopping list

--- src/components/ShoppingItem/ShoppingItem.jsx
+++ src/components/ShoppingItem/ShoppingItem.jsx
@@ -4,19 +4,43 @@ import { withStyles } from '@material-ui/core/styles'
 import { Paper, Grid, Typography } from '@material-ui/core'
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
 
 
 class ShoppingItem extends Component {
 
-  
+
+  handleCheck = () => {
+    console.log(`we checking boxes for`, this.props.id);
+
+  }
+
+  handleEdit = () => {
+    console.log(`edit clicked for`, this.props.id);
+    
+  }
+
+  handleDelete = () => {
+    console.log(`delete clicked for`, this.props.id);
+    
+  }
 
   render() {
+
 
     return (
 <>
 <TableRow>
-  <TableCell></TableCell>
+  <TableCell><Checkbox onClick={this.handleCheck}/></TableCell>
   <TableCell key={this.props.id}>{this.props.item}</TableCell>
+  <TableCell><Button 
+    variant='contained' 
+    color="primary"
+    onClick={this.handleEdit}
+    >Edit</Button></TableCell>
+  <TableCell><Button variant="contained" color="secondary">Delete</Button></TableCell>
+
 </TableRow>
 </>
     )
