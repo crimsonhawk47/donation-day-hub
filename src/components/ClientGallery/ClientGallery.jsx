143d844ada7670e2bd5afa4a16734470144ada5b
Jessica Heggem <jessicaheggem@gmail.com>
2020-03-19 14:04:32 -0500

    styling client gallery page

--- src/components/ClientGallery/ClientGallery.jsx
+++ src/components/ClientGallery/ClientGallery.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import '../App/App.css'
 import { withStyles } from '@material-ui/core/styles'
 import { Paper, Grid, Typography, Input, Button } from '@material-ui/core'
+import Fab from '@material-ui/core/Fab';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#283748',
+    },
+    secondary: {
+      main: '#6d89b1'
+    },
+    tertiary: {
+      main: '#808281'
+    },
+  },
+})
 
 const styles = theme => ({
   clientMedia: {
@@ -43,41 +59,46 @@ class ClientGallery extends Component {
   render() {
     console.log(this.state.clientId);
     console.log(this.props.match);
-    
+
 
     const { classes } = this.props;
 
     return (
-      <>
-        <h1>Upload an image to AWS S3 bucket</h1>
-        <Grid container>
-          <Input
-            id='upload-image'
-            type='file'
-            accept='image/*'
-            onChange={this.setFile} />
-          <Button variant="contained" onClick={this.uploadFile}>Upload</Button>
-          <Button variant="contained" onClick={this.displayAllFiles}>DisplayAllImages</Button>
-        </Grid>
+      <ThemeProvider theme={theme}>
+        <h1>Client Gallery</h1>
+        <p>1. Click "Choose File" to select receipts, images of clients, and videos to upload.
+          <br />
+          2. Then click "Upload" to save them.</p>
+        <br />
+        <div className="upload-btn">
+          <Grid container>
+            <Input
+              id='upload-image'
+              type='file'
+              accept='image/*'
+              onChange={this.setFile} />
+            <Fab variant="extended" color="secondary" size="small" onClick={this.uploadFile}>Upload</Fab>
+          </Grid>
+        </div>
+
         <Grid container>
           {this.props.media.map((file, index) => {
             if (file.type.split('/')[0] == 'video') {
               return (
-                  <video width="320" height="240" controls>
-                    <source src={file.link} type={file.type} />
-                      Your browser does not support the video tag.
-                    </video>
+                <video width="320" height="240" controls>
+                  <source src={file.link} type={file.type} />
+                </video>
               )
             }
-            else{
+            else {
               return <img
-              className={classes.clientMedia}
-              key={index}
-              src={file.link} />
+                className={classes.clientMedia}
+                key={index}
+                src={file.link} />
             }
           })}
         </Grid>
-      </>
+      </ThemeProvider>
     )
 
   }
