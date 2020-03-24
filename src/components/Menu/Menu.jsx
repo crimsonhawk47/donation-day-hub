7aff389f40ed4fa2b749acc53b356bf3bd39a843
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-03-23 21:32:40 -0500

    Added padding to separate comments from submit comment

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -74,6 +74,7 @@ const styles = theme => ({
   content: {
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
+    paddingBottom: '3px',
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
